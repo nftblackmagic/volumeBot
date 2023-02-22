e89b6f81cb967880b292f85c7a8b37fb1f4fb78c
An <anzhanghust@gmail.com>
2023-02-22 01:02:14 -0800

    flashloan

--- src/Flashloan.sol
+++ src/Flashloan.sol
@@ -1,11 +1,70 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier:GPl-3.0
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity ^0.8.0;
 
-contract OpenseaBot {
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IFlashLoanSimpleReceiver} from "@aave/core-v3/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {SafeMath} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
+
+contract FlashLoanAAVE is IFlashLoanSimpleReceiver, IERC721Receiver {
+    using SafeMath for uint256;
+
+    event CurrentBalance(uint256 balance);
+
+    IPoolAddressesProvider public ADRESSES_PROVIDER = IPoolAddressesProvider(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
+    IPool public POOL;
+    address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public seller = <Your wallet>;
+
+    constructor () {
+        POOL = IPool(ADRESSES_PROVIDER.getPool());
+    }
+
+    function ADDRESSES_PROVIDER() external view returns(IPoolAddressesProvider){
+        return ADRESSES_PROVIDER;
+    }
+
+    function executeFlashLoan(address asset, uint256 amount, bytes calldata params) public {
+        // perform a flashLoanSimple() on the pool contract
+        uint16 referralCode = 0;
+        POOL.flashLoanSimple(address(this), asset, amount, params, referralCode);
+    }
+
+    function callProxy(
+        address _exchange,
+        bytes calldata _calldata
+    ) public {
+        (bool success, ) = _exchange.call(_calldata);
+        require(success);
+    }
+
+    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params) external override returns(bool) { //this will be called by Pool
+
+        emit CurrentBalance(IERC20(weth).balanceOf(address(this)));
+        callProxy(address(0x00000000006c3852cbEf3e08E8dF289169EdE581),params);
+        IERC20(weth).transferFrom(seller, address(this), IERC20(weth).balanceOf(seller));
+        return true;
+    }
+
+    function approvalWeth(address _token) public {
+        IERC20(_token).approve(address(0x1E0049783F008A0085193E00003D00cd54003c71), 10000 ether); // opensea 
+        IERC20(_token).approve(address(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2), 10000 ether); // aave
+    }
     
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes memory _data
+    ) public override returns (bytes4) {
+        // IERC20(weth).transferFrom(seller, address(this), IERC20(weth).balanceOf(seller));
+        emit CurrentBalance(IERC20(weth).balanceOf(address(this)));
+        return 0x150b7a02;
+    }
 }
+
+// deployed on goerli using hardhat and the address is 0x6dc85a986Fbad7A60a2130D806F5bdF327BbD8f9
--- test/Flashloan.t.sol
+++ test/Flashloan.t.sol
@@ -1,8 +1,66 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
+import "../src/Flashloan.sol";
+import "./interface/IWETH9.sol";
 
-contract CounterTest is Test {
-    
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract FlashLoanTest is Test {
+    WETH9 WETH = WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    FlashLoanAAVE public flashLoan = new FlashLoanAAVE();
+    function setUp() public {
+        // vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 16_675_533); //fork mainnet at block 16675533
+        vm.label(address(WETH), "WETH");
+        vm.startPrank(<your wallet>);
+        // vm.stopPrank();
+    }
+
+    function fromHexChar(uint8 c) public pure returns (uint8) {
+        if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
+            return c - uint8(bytes1('0'));
+        }
+        if (bytes1(c) >= bytes1('a') && bytes1(c) <= bytes1('f')) {
+            return 10 + c - uint8(bytes1('a'));
+        }
+        if (bytes1(c) >= bytes1('A') && bytes1(c) <= bytes1('F')) {
+            return 10 + c - uint8(bytes1('A'));
+        }
+        revert("fail");
+    }
+
+    // Convert an hexadecimal string to raw bytes
+    function fromHex(string memory s) public pure returns (bytes memory) {
+        bytes memory ss = bytes(s);
+        require(ss.length%2 == 0); // length must be even
+        bytes memory r = new bytes(ss.length/2);
+        for (uint i=0; i<ss.length/2; ++i) {
+            r[i] = bytes1(fromHexChar(uint8(ss[2*i])) * 16 +
+                        fromHexChar(uint8(ss[2*i+1])));
+        }
+        return r;
+    }
+
+    function testFlashLoan() public {
+        WETH.deposit{ value: 4 ether }(); // deposit 1 eth to weth
+        WETH.transfer(address(flashLoan), 4 ether); // transfer 1 eth to flashloan contract
+        console.log("Current weth balance %s",
+          WETH.balanceOf(address(flashLoan))
+        );
+        WETH.approve(address(flashLoan), 10000 ether); // approve flashloan contract to spend weth
+
+        // buy order
+        string memory callData = '<the order from opensea>';
+        bytes memory vara = fromHex(callData);
+
+        // console.logBytes((vara));
+        flashLoan.approvalWeth(address(WETH)); 
+        flashLoan.executeFlashLoan(address(WETH), 100 ether, vara); //flashloan 1000 eth
+
+        console.log("Current weth balance %s",
+          WETH.balanceOf(address(flashLoan))
+        );
+    }
+
 }
